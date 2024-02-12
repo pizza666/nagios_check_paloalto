2aac1a35fae5133a77aa9e0612ed959429affb4b
Jan-Dirk Lehde <jan-dirk.lehde@nassmagnet.de>
2024-02-12 17:29:36 +0100

    implements certificate check over sslmgr-store

--- check_pa/modules/certificate.py
+++ check_pa/modules/certificate.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import logging
+import re
 
 import nagiosplugin as np
 from datetime import datetime
@@ -43,9 +44,12 @@ class Certificate(np.Resource):
     def __init__(self, host, token, exclude):
         self.host = host
         self.token = token
-        self.cmd = '<show><config><running>' \
-                   '<xpath>shared/certificate</xpath>' \
-                   '</running></config></show>'
+        #self.cmd = '<show><config><running>' \
+        #           '<xpath>shared/certificate</xpath>' \
+        #           '</running></config></show>'
+
+        self.cmd = '<show><sslmgr-store><config-certificate-info></config-certificate-info></sslmgr-store></show>'
+
         self.xml_obj = XMLReader(self.host, self.token, self.cmd)
         self.exclude = str(exclude).split(",")
 
@@ -57,25 +61,51 @@ class Certificate(np.Resource):
         """
         _log.info('Reading XML from: %s', self.xml_obj.build_request_url())
         soup = self.xml_obj.read()
+        _log.debug(soup)
 
-        certificates = soup.find_all('entry')
+        pattern_date = '(?<=db-exp-date:.\d{12}Z\().+.{3}\w'
+        pattern_name = '(?<=db-name: ).+'
 
-        for certificate in certificates:
-            not_valid_after = Finder.find_item(certificate,
-                                             'not-valid-after').replace(
-                "GMT", "").strip()
+        certificates_soup = soup.find('result').get_text()
+        certificate_dates = re.finditer(pattern_date,certificates_soup)
+        certificate_names = re.findall(pattern_name,certificates_soup)
+    
+    
+        idx=0
+        for cert_date in certificate_dates:
+            not_valid_after = cert_date.group(0).replace("GMT","").strip()
+            _log.info(not_valid_after)
             date_object = datetime.strptime(not_valid_after, '%b %d %H:%M:%S %Y')
             difference = date_object - get_now()
-            _log.debug('Certificate %s difference: %s days' % (
-                certificate.get('name'), difference.days))
-            try:
-                status = Finder.find_item(certificate, 'status')
-            except np.CheckError:
-                status = ""
-            if certificate.get('name') not in self.exclude:
-                if status != "revoked":
-                    yield np.Metric(certificate.get('name'), difference.days,
-                                    context='certificates')
+            _log.debug('Certificate %s difference: %s days' % (certificate_names[idx], difference.days))
+            yield np.Metric(certificate_names[idx], difference.days,context='certificates')
+            idx+=1
+
+        #    try:
+        #        status = Finder.find_item(certificate, 'status')
+        #    except np.CheckError:
+        #        status = ""
+        #    if certificate.get('name') not in self.exclude:
+        #        if status != "revoked":
+        #            yield np.Metric(certificate.get('name'), difference.days,
+        #                            context='certificates')
+
+        #for certificate in certificates:
+        #    not_valid_after = Finder.find_item(certificate,
+        #                                     'db-exp-date').replace(
+        #        "GMT", "").strip()<
+        #    date_object = datetime.strptime(not_valid_after, '%b %d %H:%M:%S %Y')
+        #    difference = date_object - get_now()
+        #    _log.debug('Certificate %s difference: %s days' % (
+        #        certificate.get('name'), difference.days))
+        #    try:
+        #        status = Finder.find_item(certificate, 'status')
+        #    except np.CheckError:
+        #        status = ""
+        #    if certificate.get('name') not in self.exclude:
+        #        if status != "revoked":
+        #            yield np.Metric(certificate.get('name'), difference.days,
+        #                            context='certificates')
 
 
 class CertificateContext(np.Context):
